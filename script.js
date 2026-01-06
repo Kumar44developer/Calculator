9ca66d4c2ee60a980cfcbb2e278a0386004ab7bf
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-06 16:33:06 +0530

    Simplified loop increment syntax in for loop

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const element = document.getElementsByTagName('li');
 const screen = document.querySelector('p');
 const clear = document.getElementById('clear');
 
+for(let i = 0;i<element.length; i+=1){ 
