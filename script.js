0fd311606c1027a46e4798831c52a6db4641ae75
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-06 16:33:36 +0530

    Added click event listener for "=" element in calculator

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const screen = document.querySelector('p');
 const clear = document.getElementById('clear');
 
 for(let i = 0;i<element.length; i+=1){ 
+ if(element[i].innerHTML === '='){
+        element[i].addEventListener("click",calculate(i));
+    }
